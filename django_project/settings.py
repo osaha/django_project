080d12a42d9d69e3eac0ff5d0b2390b91c977803
osaha <adhocosam@gmail.com>
2020-05-28 19:27:01 +0900

    second on 0528

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'blog-home'
 
 try:
     from .local_settings import *
